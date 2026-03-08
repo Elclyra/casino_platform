16143a432eef2e4bbad93cd73b8a4063119456d1
Edvard Nurk <edvardnurk@gmail.com>
2026-03-08 12:29:05 +0200

    Created initial backend with Spring boot

---
+++ backend/src/main/resources/db/migration/v1__init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users
+(
+    id         UUID PRIMARY KEY,
+    email      VARCHAR(255) NOT NULL UNIQUE,
+    name       VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
---
+++ backend/src/test/java/com/edvard/casino/CasinoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.edvard.casino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/*
+@SpringBootTest
+class CasinoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+*/
